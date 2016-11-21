fa44d459d2059aac16d85c6a869db375ead1b67c
vilhelmfalkenmark <vilhelmfalkenmark@gmail.com>
2016-11-21 09:34:25 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div className="App">
+        <h1>Comhem POC</h1>
+
+      </div>
+      </Router>
+
+
+
+    );
+  }
+}
+
+export default App;
