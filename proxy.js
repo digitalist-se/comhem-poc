d3d82fb9b47dfbb28e1c27cec3b6839ea688a79f
vilhelmfalkenmark <vilhelmfalkenmark@gmail.com>
2016-11-22 17:02:07 +0100

    proxy working

--- proxy.js
+++ proxy.js
@@ -3,25 +3,29 @@ const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 const request = require('request');
+var apiResponse = null;
 
-// var Agent = require('socks5-http-client/lib/Agent');
-
-app.use(express.static(__dirname+'/public/proxy.html'));
+/////////////////////////////////////////////
+// ALLOW CORS TO localhost:3000 dev server
+/////////////////////////////////////////////
+const allowCrossDomain = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+/////////////////////////////////////////////
+// APP CONFIGURATION
+/////////////////////////////////////////////
+app.use(allowCrossDomain);
+app.use(express.static(__dirname+'/public/proxy.json'));
 app.get("*", (req, res) => {
- res.sendFile(path.resolve(__dirname+'/public',"proxy.html"))
-
-  request
-    .get('https://api-staging.tv.comhem.se/webapi/system', options)
-    .on('response', (response) => {
-      // res.send(response.toJSON());
-      console.log(response.toJSON());
-    });
-  // res.send(request(options, callback));
-
-
+ res.sendFile(path.resolve(__dirname+'/public',"proxy.json"))
+ res.send(apiResponse);
 });
-
-
+/////////////////////////////////////////////
+// REQUEST
+/////////////////////////////////////////////
 var options = {
   url: 'https://api-staging.tv.comhem.se/webapi/channel',
   headers: {
@@ -29,19 +33,13 @@ var options = {
     'api-key': "HZvTr4YV8B"
   }
 };
-
-function callback(error, response, body) {
+const callback = (error, response, body) => {
   if (!error && response.statusCode == 200) {
-    var info = JSON.parse(body);
-    // app.set(info)
-    console.log(info);
-    // return info;
+    apiResponse = JSON.parse(body);
   } else {
-    console.log("Det funkade inte");
+    console.log(error);
   }
 }
-
 request.get(options, callback);
-
 app.listen(port);
 console.log("Server started!");
--- src/actions/channelActions.js
+++ src/actions/channelActions.js
@@ -1,48 +1,14 @@
 import axios from "axios";
 
-var instance = axios.create({
-    baseURL: 'https://api-staging.tv.comhem.se/webapi/system',
-    method: 'get', // default
-    headers: {
-        'webapi-version' : 99,
-        'api-key': "HZvTr4YV8B"
-    }
-})
-var config = {
-  headers: {
-      'webapi-version' : "99",
-      'api-key': "HZvTr4YV8B"
-  }
-};
-
 export function getChannels() {
   return function(dispatch) {
-
-    // axios.get("https://api-staging.tv.comhem.se/webapi/system")
-    // .then((response) => {
-    //   dispatch({type: "CHANNELS_FETCHED", payload: response.data})
-    // })
-    // .catch((err) => {
-    //   dispatch({type: "CHANNELS_REJECTED", payload: err})
-    //   })
-
-    axios.get('https://api-staging.tv.comhem.se/webapi/system', config)
+    // axios.get("localhost:8080")
+    axios.get("http://localhost:8080")
     .then((response) => {
-      console.log(response.data);
-      console.log(response.status);
-      console.log(response.headers);
-
-
-      // dispatch({type: "CHANNELS_FETCHED", payload: response.data})
+      dispatch({type: "CHANNELS_FETCHED", payload: response.data})
     })
     .catch((err) => {
-      console.log("Det blev en error");
-      dispatch({type: "CHANNELS_REJECTED", payload: err})
-
+      dispatch({type: "EVENTS_REJECTED", payload: err})
     })
-
-
-
-
   }
 }
